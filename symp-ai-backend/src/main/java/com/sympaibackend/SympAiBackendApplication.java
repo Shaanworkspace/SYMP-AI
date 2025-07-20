b580bb084a230794b04a078dca47c89a31be54d8
shaan Yadav <shaanyworkspace@gmail.com>
2025-07-20 10:46:45 +0530

    added backend and started

---
+++ symp-ai-backend/src/main/java/com/sympaibackend/SympAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sympaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SympAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SympAiBackendApplication.class, args);
+	}
+
+}
---
+++ symp-ai-backend/src/test/java/com/sympaibackend/SympAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sympaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SympAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
